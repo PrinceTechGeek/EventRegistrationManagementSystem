0b387215b09e11607483192c01a2f015cca3ae6b
Prince Sharma <65114732+PrinceTechGeek@users.noreply.github.com>
2024-09-13 19:28:42 +0530

    Initial Commit
    Event Registration Management system

---
+++ EventRegistrationService.MVC/Controllers/RegistrationController.cs
@@ -0,0 +1,66 @@
+﻿using EventRegistrationSystem.Application.Interfaces;
+using EventRegistrationSystem.Domain.DTOs;
+using EventRegistrationSystem.MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace EventRegistrationSystem.MVC.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        private readonly IRegistrationService _registrationService;
+
+        public RegistrationController(IRegistrationService registrationService)
+        {
+            _registrationService = registrationService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var registrationDtos = await _registrationService.GetAllRegistrationsAsync();
+
+            var registrationViewModels = registrationDtos.Select(dto => new RegistrationViewModel
+            {
+                EventName = dto.EventName,
+                ParticipantName = dto.ParticipantName,
+                EmailAddress = dto.EmailAddress,
+                NumberOfTickets = dto.NumberOfTickets,
+                EventType = dto.EventType
+            }).ToList();
+
+            return View(registrationViewModels);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View(new RegistrationViewModel());
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(RegistrationViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var registrationDto = new RegistrationDto
+                {
+                    EventName = model.EventName,
+                    ParticipantName = model.ParticipantName,
+                    EmailAddress = model.EmailAddress,
+                    NumberOfTickets = model.NumberOfTickets,
+                    EventType = model.EventType
+                };
+
+                await _registrationService.AddRegistrationAsync(registrationDto);
+                return RedirectToAction("Success");
+            }
+            return View(model);
+        }
+
+        public IActionResult Success()
+        {
+            return View();
+        }
+    }
+}
