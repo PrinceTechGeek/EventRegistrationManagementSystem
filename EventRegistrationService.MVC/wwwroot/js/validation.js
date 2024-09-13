05327dc67bf288a684ef664732e507401cba47e9
Prince Sharma <65114732+PrinceTechGeek@users.noreply.github.com>
2024-09-13 20:39:02 +0530

    Repo Copy
    Copying to new repo

---
+++ EventRegistrationService.MVC/wwwroot/js/validation.js
@@ -0,0 +1,35 @@
+﻿$(document).ready(function () {
+    $("#registrationForm").validate({
+        rules: {
+            EventName: "required",
+            ParticipantName: "required",
+            EmailAddress: {
+                required: true,
+                email: true
+            },
+            NumberOfTickets: {
+                required: true,
+                digits: true,
+                min: 1
+            },
+            EventType: "required"
+        },
+        messages: {
+            EventName: "Please enter event name",
+            ParticipantName: "Please enter participant name",
+            EmailAddress: {
+                required: "Please enter a valid email address",
+                email: "Please enter a valid email address"
+            },
+            NumberOfTickets: {
+                required: "Please enter a valid number of tickets",
+                digits: "Please enter a valid number of tickets",
+                min: "Number of tickets must be greater than 0"
+            },
+            EventType: "Please select an event type"
+        },
+        errorPlacement: function (error, element) {
+            error.appendTo(element.closest("div").find("span.text-danger"));
+        }
+    });
+});
